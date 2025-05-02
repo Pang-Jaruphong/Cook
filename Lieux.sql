11de142d1f43df06ca67a69accab88bb7454c29f
Pang-Jaruphong <Jaruphong.khruasarn@eduvaud.ch>
2025-05-02 08:19:18 +0200

    Insert tables de base
    les trois tables

---
+++ Lieux.sql
@@ -0,0 +1,5 @@
+INSERT INTO `places` (id,name, address, town, phone) VALUES (1,'Thai Siri Restaurant','Rue de la gare 12', 'Ste. Croix', '0243800790');
+INSERT INTO `places` (id,name, address, town, phone) VALUES (2,'Mont Blanc','Rue de Lausanne 3', 'Yverdon-les-bains', '0245556070');
+INSERT INTO `places` (id,name, address, town, phone) VALUES (3,'Turkey Kitchen','Rue de Génève 12', 'Yverdon-les-bains', '0245581412');
+INSERT INTO `places` (id,name, address, town, phone) VALUES (4,'Asia Shop','Rue César Roux 2', 'Lausanne', '0213161212');
+INSERT INTO `places` (id,name, address, town, phone) VALUES (5,'Tong Bay Restaurant','Grand Rue 5', 'Payerne', '0266002535');
